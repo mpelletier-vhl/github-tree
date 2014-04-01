48bf8f32a495b4d70e4e2c9d431a5fd34442c98a
Michael Pelletier <yoricksrequiem@gmail.com>
2014-03-31 20:37:51 -0400

    Temporarily disable caching.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  OAuth.initialize('5K2-WvCelwL0WSroLuUViEz4gpU', options = {cache: true});
+  OAuth.initialize('5K2-WvCelwL0WSroLuUViEz4gpU', options = {cache: false});
 
 	OAuth.popup('github', function(error, result) {
 		chrome.runtime.sendMessage({ messages: result});
