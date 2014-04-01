68f4dc434cca4d072483bd63d5eba3ef2200d4aa
Michael Pelletier <yoricksrequiem@gmail.com>
2014-03-31 20:00:15 -0400

    Use correct public key

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-  OAuth.initialize('OzXQoIVWxW526_qt8cp8R2kyPaU', options = {cache: true});
+  OAuth.initialize('5K2-WvCelwL0WSroLuUViEz4gpU', options = {cache: true});
 
 	OAuth.popup('github', function(error, result) {
 		console.log(result);
