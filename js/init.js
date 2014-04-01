12c6c95e57ac80e305582151f0da8d1f02e0fe61
Michael Pelletier <yoricksrequiem@gmail.com>
2014-03-31 20:31:27 -0400

    Remove console log, add message passing.

--- js/init.js
+++ js/init.js
@@ -1,9 +1,7 @@
-
 $(document).ready(function() {
   OAuth.initialize('5K2-WvCelwL0WSroLuUViEz4gpU', options = {cache: true});
 
 	OAuth.popup('github', function(error, result) {
-		console.log(result);
+		chrome.runtime.sendMessage({ messages: result});
 	});
-
 });
